20100c8595bf4adf036420f44cb27815eb707e30
Time4Coding <142026898+Time4Coding@users.noreply.github.com>
2023-08-25 14:12:16 +0100

    fix error

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CategoryCard from './components/CategoryCard';
-const data = 10
+
 function App() {
   return (
     <div className="App">
       <h1> Hello Gomycode!</h1>
-      <CategoryCard data ={data}/>
+      <CategoryCard />
     </div>
   );
 }
